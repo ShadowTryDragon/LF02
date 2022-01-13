50eda54147b56218ddc0baadd26c6bbead8f0c49
ShadowTryDragon <ShadowTryDragon>
2022-01-13 15:20:34 +0100

    .

---
+++ Obst/src/Obst.java
@@ -0,0 +1,37 @@
+public abstract class Obst {
+    private String name;
+    private String farbe;
+    private double gewicht;
+    private boolean reif;
+
+
+
+    public  Obst(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getFarbe() {
+        return farbe;
+    }
+
+    public void setFarbe(String farbe) {
+        this.farbe = farbe;
+    }
+
+    public double getGewicht() {
+        return gewicht;
+    }
+
+    public void setGewicht(double gewicht) {
+        this.gewicht = gewicht;
+    }
+
+    public boolean getReife(boolean reif) {
+        return reif;
+    }
+    public void setReife(boolean reif) {
+        this.reif = reif;
+}}
---
+++ Obst/src/Programm.java
@@ -0,0 +1,20 @@
+public class Programm  {
+    public static void main(String[] args) {
+         Apfel apfel = new apfel();
+
+        Apfel.setFarbe("Grün");
+        Apfel.setGewicht(250);
+        Apfel.setReife(true);
+
+        System.out.println(Apfel.getName());
+        System.out.println(Apfel.getGewicht());
+ Banane banane = new Banane();
+ banane.setKruemmung(0.5);
+        System.out.println(banane.getName());
+        System.out.println(banane.getKruemmung());
+
+    }
+
+
+
+}
