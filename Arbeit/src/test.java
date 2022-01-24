90f4ca283a719b37fa1c6d58d7668d78ce2855bd
ShadowTryDragon <ShadowTryDragon>
2022-01-24 11:14:00 +0100

    .

--- Arbeit/src/test.java
+++ Arbeit/src/test.java
@@ -3,10 +3,12 @@ public class test {
         Spieler horst1 = new Spieler("horst1",20,10,5);
         Torwart horst2 = new Torwart("horst2",13,6,9,5);
         Trainer horst3 = new Trainer("horst3",19,9);
+        Spieler test = new Spieler("Mani",18,3,10);
 
-        System.out.println(horst1.getName());
-        System.out.println(horst2.getName());
-        System.out.println(horst3.getErfahrung());
+        System.out.println("Name: " + horst1.getName());
+        System.out.println("Alter: " + horst1.getAlter());
+        System.out.println("Motivation: " + horst1.getMotivation());
+        System.out.println("Stärke: " + test.getStaerke());
     }
 
 
