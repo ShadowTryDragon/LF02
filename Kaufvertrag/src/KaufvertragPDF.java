fc503e4c3873bbdf89d3eb6f4f8f5ffc8c1da54a
ShadowTryDragon <ShadowTryDragon>
2022-03-22 11:08:58 +0100

    .

--- Kaufvertrag/src/KaufvertragPDF.java
+++ Kaufvertrag/src/KaufvertragPDF.java
@@ -12,22 +12,24 @@ public class KaufvertragPDF   {
     public static void main(String[] args) throws  IOException {
 
         String datei = "H:/Dokumente/Kaufvertrag.pdf";
+
         // PdfWriter
         PdfWriter writer = new PdfWriter(datei);
         PdfDocument pdfDocument = new PdfDocument(writer);
         Document document = new Document(pdfDocument);
         PdfFont font = PdfFontFactory.createFont(StandardFonts.HELVETICA);
+
         // Formatierung
         document.setFont(font);
         document.setFontSize(12);
 
 
         //Paragraph 1
-        Paragraph p1 = new Paragraph("Vertragspartner").setFontColor(new DeviceRgb(50,100,150)).setFontSize(20);
+        Paragraph p1 = new Paragraph("Vertragspartner").setFontColor(new DeviceRgb(50,100,150)).setFontSize(20).setBold();
         p1.add("\n");
         p1.add("\n");
         //Paragraph 2
-        Paragraph p2 = new Paragraph("Verkäufer ").setFontColor(new DeviceRgb(100,100,200)).setFontSize(16);
+        Paragraph p2 = new Paragraph("Verkäufer ").setFontColor(new DeviceRgb(100,100,200)).setFontSize(16).setBold();
        //Paragraph 3
 
 
@@ -39,7 +41,7 @@ public class KaufvertragPDF   {
 
 
         // Paragraph 4
-        Paragraph p4 = new Paragraph("Käufer ").setFontColor(new DeviceRgb(70,80,90)).setFontSize(16);
+        Paragraph p4 = new Paragraph("Käufer ").setFontColor(new DeviceRgb(70,80,90)).setFontSize(16).setBold();
         p4.add("\n");
         //Paragraph 5
         Paragraph p5 = new Paragraph("Name: Klaus Brandt");
@@ -48,17 +50,22 @@ public class KaufvertragPDF   {
         p5.add("\n");
         p5.add("Adressse: Zu Hause 3a 28199 Bremen");
         //Paragraph 6
-        Paragraph p6 = new Paragraph("Vekaufte Ware").setFontColor(new DeviceRgb(75,40,90)).setFontSize(16);
+        Paragraph p6 = new Paragraph("Vekaufte Ware").setFontColor(new DeviceRgb(75,40,90)).setFontSize(16).setBold();
         p6.add("\n");
         //Paragraph 7
         Paragraph p7 = new Paragraph("Laptop").setFontColor(new DeviceRgb());
+        p7.add("\n");
         p7.add("\n");
         p7.add("Krasser High End Laptop");
         p7.add("\n");
         p7.add("Preis: 250 Zacken");
         p7.add("\n");
         p7.add("nur Zahlung in Bar");
+        p7.add("\n");
+        p7.add("\n");
 
+        //Paragraph 8
+        Paragraph p8 = new Paragraph(" Achtung Keine Rückerstattung möglich!").setFontSize(6).setBold().setUnderline();
 
 
 
@@ -71,6 +78,7 @@ public class KaufvertragPDF   {
         document.add(p5);
         document.add(p6);
         document.add(p7);
+        document.add(p8);
         document.close();
 
 
